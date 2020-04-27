fe051176f8d3613341b1dbd2c8e7b529c4e5abd5
s18895 <61684706+s18895@users.noreply.github.com>
2020-04-27 11:33:30 +0200

    Commit

---
+++ New Unity Project/Assets/Scripts/CanonBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonBall : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = transform.right * -1 * speed;
+    }
+    IEnumerator OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("MyPlayer"))
+        {
+            Destroy(collision.gameObject);
+            yield return new WaitForSeconds(2);
+            Destroy(gameObject);
+        }
+    }
+}
